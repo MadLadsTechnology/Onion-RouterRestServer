83d86ff7c69bf96cb0d9995404cbb8e885e5a5c7
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-21 10:29:11 +0100

    Changed: No more hashmaps, removed unessecary methods

--- src/main/java/ntnu/idatt2104/madlads/nodeServerAPI/controller/NodeAPI.java
+++ src/main/java/ntnu/idatt2104/madlads/nodeServerAPI/controller/NodeAPI.java
@@ -1,8 +1,5 @@
 package ntnu.idatt2104.madlads.nodeServerAPI.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import ntnu.idatt2104.madlads.nodeServerAPI.model.Node;
 import ntnu.idatt2104.madlads.nodeServerAPI.model.NodeList;
@@ -11,11 +8,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
@@ -23,25 +18,19 @@ public class NodeAPI {
     Logger logger = LoggerFactory.getLogger(NodeAPI.class);
     NodeList nodeList= new NodeList();
 
-    @Autowired
-    private ObjectMapper mapperBuilder;
-
     @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping(value="/getAllNodes", method= RequestMethod.GET)
     public String getAllNodes() throws JSONException {
 
-        HashMap<String, Node> list = nodeList.getListOfAllNodes();
-        ArrayList<String> listOfAllKeys = new ArrayList<>(list.keySet());
+        ArrayList<Node> list = nodeList.getListOfAllNodes();
 
         JSONArray array = new JSONArray();
 
-        for (String key: listOfAllKeys) {
-            Node node = list.get(key);
+        for (Node node: list) {
 
             JSONObject jo = new JSONObject();
             jo.put("port", node.getPort());
             jo.put("ip", node.getAddress());
-            jo.put("publicKey", key);
 
             array.put(jo);
         }
@@ -55,27 +44,13 @@ public class NodeAPI {
     @PostMapping ("/putNode")
     public void putNode(@RequestBody ObjectNode payload) {
 
-        String pubKey  = payload.get("publicKey").asText();
+        Node node = new Node(payload.get("address").asText());
 
-        String[] splitString = payload.get("address").asText().split(":");
-
-        if (nodeList.addNode(pubKey, new Node(Integer.parseInt(splitString[1]), splitString[0]))){
-            logger.info("Added node with address: " + splitString[1] +":"+splitString[0] +"\nand public key: " + pubKey);
+        if (nodeList.addNode(node)){
+            logger.info("Added node with address: " + node);
         }else{
-            logger.info("Node with address" + splitString[1] +":"+splitString[0] +"Already exists");
+            logger.info("Node with address" + node +"Already exists");
         }
-    }
-
-    @CrossOrigin(origins = "http://localhost:8080")
-    @GetMapping(value="/getAddress")
-    public String getAddressOfSpecifiedNode(@RequestParam String payload){
-        String address = nodeList.getAddressOfSpecifiedNode(payload);
-        if(address.equals("")){
-            logger.info("the given request could not be found");
-        }else {
-            logger.info("Address returned: " + address);
-        }
-        return address;
     }
 
     @CrossOrigin(origins = "http://localhost:8080")

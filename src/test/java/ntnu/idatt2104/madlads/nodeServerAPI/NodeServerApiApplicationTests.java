bbf8ced163275822fd1de826598d6e904699435e
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-21 14:46:28 +0100

    Fixed: tests now work

--- src/test/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplicationTests.java
+++ src/test/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplicationTests.java
@@ -6,87 +6,59 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+
+import static ntnu.idatt2104.madlads.nodeServerAPI.APIService.*;
 
 @SpringBootTest
 class NodeServerApiApplicationTests {
+
 	String JSON_DATA;
 	NodeList nodes = new NodeList();
-	@BeforeEach
-	public void startup() {
-	}
 
 	@Test
-	public void postNodeCheck() throws Exception {
-
-		apiPOSTNode("http://localhost:8080/api/putNode", "YOU WERE THE CHOSEN ONE! It was said that you would destroy the Sith, not join them, bring balance to the force, not leave it in darkness", "localhost:" + 1234);
-		apiPOSTNode("http://localhost:8080/api/putNode", "Don't lecture me, Obi-Wan. I see through the lies of the Jedi! I do not fear the dark side as you do. I have brought peace, freedom, justice, and security to my new empire!", "localhost:" + 12378);
-		apiPOSTNode("http://localhost:8080/api/putNode", "Don't lecture me, Obi-Wan. I see through the lies of the Jedi! I do not fear the dark side as you do. I have brought peace, freedom, justice, and security to my new empire!", "localhost:" + 12379);
-
-		System.out.println("Postet noder");
+	public void postNodeTest() throws Exception {
+		removeAllNodes("http://localhost:8080/api/deleteAllNodes");
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 1234);
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 12378);
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 12378);
 		JSON_DATA = apiGETRequest("http://localhost:8080/api/getAllNodes");
-
 		fillNodes();
 		ArrayList<Node> listOfAllNodes = nodes.getListOfAllNodes();
 		Assertions.assertEquals(2, listOfAllNodes.size());
-
+		Assertions.assertEquals("localhost:1234", listOfAllNodes.get(0).toString());
 	}
 
-	public static void apiPOSTNode(String url, String publicKey, String address) throws Exception {
+	@Test
+	public void deleteNodeTest() throws Exception {
+		removeAllNodes("http://localhost:8080/api/deleteAllNodes");
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 1234);
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 12378);
 
-		URL urlPost = new URL(url);
-		HttpURLConnection http = (HttpURLConnection) urlPost.openConnection();
-		http.setRequestMethod("POST");
-		http.setDoOutput(true);
-		http.setRequestProperty("Content-Type", "application/json");
+		apiDELETENode("http://localhost:8080/api/deleteNode", "localhost:" + 1234);
+		JSON_DATA = apiGETRequest("http://localhost:8080/api/getAllNodes");
+		fillNodes();
 
-		String data = "{ \"publicKey\":\""  + publicKey +"\", \"address\":\"" + address + "\"}";
-
-		byte[] out = data.getBytes(StandardCharsets.UTF_8);
-
-		OutputStream stream = http.getOutputStream();
-		stream.write(out);
-
-		System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
-		http.disconnect();
+		ArrayList<Node> listOfAllNodes = nodes.getListOfAllNodes();
+		Assertions.assertEquals(1, listOfAllNodes.size());
 	}
 
-	public static String apiGETRequest(String url) throws Exception {
-		URL urlForGetRequest = new URL(url);
-		return getString(urlForGetRequest);
+	@Test
+	public void deleteAllNodes() throws Exception {
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 1234);
+		apiPOSTNode("http://localhost:8080/api/putNode", "localhost:" + 12378);
+		removeAllNodes("http://localhost:8080/api/deleteAllNodes");
+		JSON_DATA = apiGETRequest("http://localhost:8080/api/getAllNodes");
+		fillNodes();
+
+		ArrayList<Node> listOfAllNodes = nodes.getListOfAllNodes();
+		Assertions.assertEquals(0, listOfAllNodes.size());
 	}
 
-	private static String getString(URL urlForGetRequest) throws Exception {
-		HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
-		connection.setRequestMethod("GET");
-		int responseCode = connection.getResponseCode();
 
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuilder response = new StringBuilder();
-			String readLine;
-			while ((readLine = in.readLine()) != null) {
-				response.append(readLine);
-			}
-			in.close();
-
-			return String.valueOf(response);
-
-		} else {
-			throw new Exception("Could not connect");
-		}
-	}
 
 	private void fillNodes() throws ParseException {
 		JSONParser parser = new JSONParser();
@@ -99,11 +71,9 @@ class NodeServerApiApplicationTests {
 
 	private void parseNodeObject(JSONObject node){
 		//Get node's public key
-		String publicKey = (String) node.get("publicKey");
-		String host = (String) node.get("host");
-		System.out.println(node.get("port"));
-		int port = Integer.parseInt(String.valueOf((long) node.get("port")));
+		String host = (String) node.get("ip");
+		int port = Integer.parseInt(String.valueOf((long) node.get("port")));;
 
-		nodes.addNode(new Node(port , host));
+		nodes.addNode(new Node(port, host));
 	}
 }

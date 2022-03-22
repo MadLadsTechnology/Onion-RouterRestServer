ff1631ac3cfe35eed69bec2104359bb95ad3f4ba
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-22 14:40:39 +0100

    Added: outputs localhost

--- src/main/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplication.java
+++ src/main/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplication.java
@@ -1,16 +1,25 @@
 package ntnu.idatt2104.madlads.nodeServerAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class NodeServerApiApplication {
 
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws UnknownHostException {
+		Logger logger = LoggerFactory.getLogger(NodeServerApiApplication.class);
 		SpringApplication.run(NodeServerApiApplication.class, args);
+		logger.info("Address of this server: "+ InetAddress.getLocalHost());
 	}
 
 	@Bean

c17a1f7144d44cae3850e68d2685faf6e6c5de49
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-22 16:32:01 +0100

    Removed unused class and optimized imports

--- src/test/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplicationTests.java
+++ src/test/java/ntnu/idatt2104/madlads/nodeServerAPI/NodeServerApiApplicationTests.java
@@ -3,28 +3,10 @@ package ntnu.idatt2104.madlads.nodeServerAPI;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import ntnu.idatt2104.madlads.nodeServerAPI.controller.NodeAPI;
-import ntnu.idatt2104.madlads.nodeServerAPI.model.Node;
-import ntnu.idatt2104.madlads.nodeServerAPI.model.NodeList;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.lang.reflect.Array;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @SpringBootTest
 class NodeServerApiApplicationTests {
